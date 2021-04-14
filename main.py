0d46ce478d38bab04fdc0404fe15bf96b4f0c74d
Valeriy Karagaev <valeriy.karagaev@mail.ru>
2021-04-14 12:40:08 +0300

    create bot

---
+++ main.py
@@ -0,0 +1,17 @@
+import telebot
+from settings import TG_TOKEN
+
+bot = telebot.TeleBot(TG_TOKEN)
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == 'Привет':
+        bot.send_message(message.from_user.id, 'Привет, чем могу помочь?')
+    elif message.text == '/help':
+        bot.send_message(message.from_user.id, 'Напиши привет')
+    else:
+        bot.send_message(message.from_user.id, 'Я тебя не понимаю, напиши /help')
+
+
+bot.polling(none_stop=True, interval=0)
