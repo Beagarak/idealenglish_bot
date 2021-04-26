0cb3dbada1b1255d8721d206b65779e005b0a50a
sohazasoha <70949593+sohazasoha@users.noreply.github.com>
2021-04-26 12:28:35 +0300

    Update main

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import telebot
 from settings import TG_TOKEN
+import buttons
 
 bot = telebot.TeleBot(TG_TOKEN)
 
 
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    buttons.main_menu(message)
+
+
 @bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == 'Привет':
-        bot.send_message(message.from_user.id, 'Привет, чем могу помочь?')
-    elif message.text == '/help':
-        bot.send_message(message.from_user.id, 'Напиши привет')
+def basic_actions(message):
+    if message.text == buttons.add_word_button:
+        bot.send_message(message.from_user.id,
+                         'Можешь добавить своё слово! (в разработке)')
+    elif message.text == buttons.check_knowledge_button:
+        bot.send_message(message.from_user.id,
+                         'Пора проверить твои знания (в разработке)')
+    elif message.text == buttons.learn_words_button:
+        bot.send_message(message.from_user.id,
+                         'Давай поучим новые слова! (в разработке)')
+    elif message.text == buttons.translate_button:
+        bot.send_message(message.from_user.id,
+                         'Введи слово, которое хочешь перевести:')
+    elif message.text == buttons.statistic_button:
+        bot.send_message(message.from_user.id,
+                         'Вот твоя статистика: (в разработке)')
     else:
-        bot.send_message(message.from_user.id, 'Я тебя не понимаю, напиши /help')
+        bot.send_message(message.from_user.id,
+                         'Не понимаю, используй клавиатуру')
 
 
 bot.polling(none_stop=True, interval=0)
