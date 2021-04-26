04f2870f7b1fd6763891e492f5c7ca8ec0835a58
Есаулов Лев <70949524+auto3181@users.noreply.github.com>
2021-04-26 15:40:32 +0300

    Вам точно понравится

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import telebot
 from settings import TG_TOKEN
+import trans_alg
 
 bot = telebot.TeleBot(TG_TOKEN)
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == 'Привет':
-        bot.send_message(message.from_user.id, 'Привет, чем могу помочь?')
-    elif message.text == '/help':
-        bot.send_message(message.from_user.id, 'Напиши привет')
-    else:
-        bot.send_message(message.from_user.id, 'Я тебя не понимаю, напиши /help')
+    type_of_lang = int(ord(message.text[0]))
+    translation = trans_alg.get_translate(type_of_lang, message.text)
+    bot.send_message(message.from_user.id, translation)
+    bot.send_photo(chat_id=message.chat.id, photo=trans_alg.get_picture(translation))
 
 
 bot.polling(none_stop=True, interval=0)
