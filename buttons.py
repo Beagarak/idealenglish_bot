c6da16afe179e37b325ba29a6c9c455fae8ded91
Valeriy Karagaev <valeriy.karagaev@mail.ru>
2021-05-12 12:29:36 +0300

    decorate buttons

--- buttons.py
+++ buttons.py
@@ -2,11 +2,12 @@ import telebot
 from settings import TG_TOKEN
 
 bot = telebot.TeleBot(TG_TOKEN)
-add_word_button = 'Добавить слова'
-learn_words_button = 'Учить слова'
-check_knowledge_button = 'Проверка знаний'
-statistic_button = 'Статистика'
-translate_button = 'Переводчик'
+add_word_button = '|Добавить слова|'
+learn_words_button = '|Учить слова|'
+check_knowledge_button = '|Проверка знаний|'
+statistic_button = '|Статистика|'
+translate_button = '|Переводчик|'
+
 
 
 @bot.message_handler(commands=['start'])
@@ -16,5 +17,5 @@ def main_menu(message):
     user_markup.row(learn_words_button)
     user_markup.row(check_knowledge_button)
     user_markup.row(statistic_button, translate_button)
-    bot.send_message(message.from_user.id, 'Добро пожаловать ',
+    bot.send_message(message.from_user.id, 'Добро пожаловать',
                      reply_markup=user_markup)
