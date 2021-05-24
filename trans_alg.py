dd1a194a080f082b1947ff5622b3786a5641646b
Есаулов Лев <70949524+auto3181@users.noreply.github.com>
2021-05-24 17:11:13 +0300

    With docstring

--- trans_alg.py
+++ trans_alg.py
@@ -1,3 +1,8 @@
+## Основной файл проекта
+#
+# Реализует операции перевода слов
+# @file trans_alg.py
+# @author Карагаев Валерий, Олейник Александр, Есаулов Лев, Пеева Софья, Титова Екатерина
 from translate import Translator
 import telebot
 from settings import TG_TOKEN
@@ -6,12 +11,11 @@ import buttons
 bot = telebot.TeleBot(TG_TOKEN)
 
 
+## Обрабатывает слово(или текст) для перевода
+# @param asci Аски код первого символа текста, который мы должны перевести
+# @param text Слово(или текст), который мы должны перевести
+# @return Возвращает список слов длинной 3, где первое слово - англ., второе - русс., третье - переведенное слово
 def get_translate(asci, text):
-    """
-    :param asci: Аски код первого символа текста, который мы должны перевести
-    :param text: Слово(или текст), который мы должны перевести
-    :return: Список слов длинной 3, где первое слово - англ., второе - русс., третье - переведенное слово
-    """
     trans_and_lang = []
     trans_and_lang.clear()
     translation = ''
@@ -30,6 +34,9 @@ def get_translate(asci, text):
     return trans_and_lang
 
 
+## Находит картинку через "Яндекс"
+# @param text Слово(или текст) полученное от пользователя
+# @return Возвращает ссылку на картинку
 def get_picture(text):
     """
     :param text: Слово(или текст), который мы должны перевести
@@ -39,12 +46,11 @@ def get_picture(text):
     return link
 
 
+## Обрабатывает слово(или текст) и отправляет его перевод с картинкой пользователю
+# @param user_word Слово(или текст) полученное от пользователя
+# @param user_id Адрес пользователя, на который посылаем сообщение
+# @return возвращает Список из англ и русского слова
 def translation_function(user_word, user_id):
-    """
-    :param user_word: Слово(или текст) полученное от пользователя
-    :param user_id: адрес пользователя, на который посылаем сообщение
-    :return: Отправляет переведенное слово и картинку пользователю
-    """
     type_of_lang = int(ord(user_word[0]))
     log_words = get_translate(type_of_lang, user_word)
     translation = log_words[2]
